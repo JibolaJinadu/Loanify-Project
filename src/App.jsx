b028750d1fb168f9c0ba2ad8e125a675d1fbdf53
Adeyera <asimolowoelizabeth@gmail.com>
2023-05-25 11:16:22 +0100

    password Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Login from './login/Login';
 import SignUp from './signUp/SignUp';
+import ResetPassword from './passwordReset/ResetPassword';
 
 function App() {
   return (
     <>
+      
       <Login />
+      <ResetPassword />
+      
       {/* <SignUp /> */}
     </>
   );
