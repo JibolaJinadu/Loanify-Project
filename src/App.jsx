3a25647828e200fb7e2d472e1b2fea9aa7201c19
Adeyera <asimolowoelizabeth@gmail.com>
2023-06-02 16:05:47 +0100

    Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Token from './tokenAuth/Token';
 import Home from './home/Home';
 import SignUp from './signUp/SignUp';
 import EmailSent from './signUp/EmailSent';
-import Loan from './loan/Loan'
+import Loan from './loan/Loan';
+import ProfileNav from './Profile/ProfileNav';
 
 // import OverviewPage from "./overview/OverviewPage";
 // import SupportPage from "./support/SupportPage";
@@ -17,6 +18,7 @@ function App() {
     <>
       {/* <OverviewPage/> */}
       {/* <SupportPage/> */}
+      <ProfileNav />
 
       <Router>
         <Routes>
@@ -26,12 +28,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/reset-password" element={<ResetPassword />}></Route>
           <Route path="/settings" element={<Settings />}></Route>
-<<<<<<< HEAD
-          <Route path="/token" element={<Token />}></Route>
-          <Route path="/loan" element={<Loan />}></Route>
-=======
           <Route path="/verification" element={<Token />}></Route>
->>>>>>> b20ae27e76b57362b3b9859c65a8c97a5958c45c
         </Routes>
       </Router>
     </>
