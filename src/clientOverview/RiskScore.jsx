96cb21cdc09b4aa1b988514625b6498333a119e4
Dolapo Ojulawo <116099457+DolapoOjulawo1@users.noreply.github.com>
2023-06-30 01:47:35 +0100

    Updated RiskScore

--- src/clientOverview/RiskScore.jsx
+++ src/clientOverview/RiskScore.jsx
@@ -1,41 +1,44 @@
 import React from 'react';
 import './RiskScore.css';
-import risk from './img/Risk.png';
+import HalfCircleChart from './HalfCircleChart';
+
 
 const RiskScore = () => {
   return (
-    <div>
-      <div className="risk-score">
-        <div className="rs-sizeb">
+    <div className='risk-space'>
+      <div className="risk-container">
+        <div className="risk-score">
           <p>Risk Score:</p>
           <p>Risk Level:</p>
           <p>System Decision:</p>
-          <p>Odds(Good:Bad):</p>
-          <p>Default Probability:</p>
+          <p>Odds (Good:Bad):</p>
+          <h5>Default Probability:</h5>
         </div>
-        <div className='rs-space'></div>
-        <div className="rs-size">
+
+        <div className="risk-grade risk-space">
           <p>700/800</p>
           <p>Low</p>
           <p style={{ color: 'green' }}>Approve</p>
           <p>80:20</p>
           <p style={{ color: 'green' }}>15%</p>
         </div>
-        <div className='risk-space'></div>
-        <div>
-          <img src={risk} alt="image" style={{ width: '250px' }}></img>
-        </div>
+        <div></div>
+        <p>
+        <HalfCircleChart/>
+      </p>
       </div>
       <hr />
-      <div className="risk-score">
+      <div className="comment-container">
         <p className='risk-comment'>Decision Comments:</p>
-        <div className='rs-space'></div>
-        <div className="rs-highlighted">
-          <p className='riskComment'>1. System recommends making a standard client check</p>
-          
+        <div className='risk-space'>
+        <div className="risk-highlighted">
+          <p className='risk-feedback'>1. System recommends making a standard client check</p> 
         </div>
+        </div>
+        
       </div>
     </div>
+  
   );
 };
 
